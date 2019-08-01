149446828851e0ba9f92096960127d13efea7b3f
wannasa0199 <52033980+wannasa0199@users.noreply.github.com>
2019-08-01 13:08:35 +0700

    Create main.py

---
+++ assignment5/main.py
@@ -0,0 +1,11 @@
+from flask import flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+  return render_template('login.html')
+  
+if __name__ == '__main__':
+  app.run(debug=Turn)
