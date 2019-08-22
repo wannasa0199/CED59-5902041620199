1f11f616a78f587b95fec61eb28dd0debd63e587
Computer Education <>
2019-08-22 17:12:55 +0700

    Ud assignment8

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,13 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template , request
+from wtforms import StringField,PasswordField
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Length, Email
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'any secret string'
+
+class RegisterForm(FlaskForm):
+    name = StringField("name", validators=[InputRequired()])
+    last = StringField("last", validators=[InputRequired()])
+    email = StringField("Email",  [InputRequired("Please enter your email address."), Email("Please enter your email again.")])
+    username = StringField("username", validators=[InputRequired()])
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8,max=12, message="Please enter your password 8-12 characters.")])
 
 @app.route('/')
+def member():
+    form = RegisterForm()
+    return render_template('index.html', form=form)
+
+
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    return render_template('index.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return "Register OK"
+    return render_template('login.html', form=form)
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
