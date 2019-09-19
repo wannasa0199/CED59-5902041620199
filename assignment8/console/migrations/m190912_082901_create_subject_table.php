d4ac44532c1c1245e9404d555f8023caca16905e
wannasa0199 <52033980+wannasa0199@users.noreply.github.com>
2019-09-19 13:38:27 +0700

    Update m190912_082901_create_subject_table.php

--- assignment8/console/migrations/m190912_082901_create_subject_table.php
+++ assignment8/console/migrations/m190912_082901_create_subject_table.php
@@ -14,6 +14,9 @@ class m190912_082901_create_subject_table extends Migration
     {
         $this->createTable('{{%subject}}', [
             'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'section' => $this->string(),
+            'teach_id' => $this->integer()        
         ]);
     }
 
