f93b38b1c9f1e56e1bb92dd2c49df5c1dafb4275
Computer Education <>
2019-09-19 14:04:49 +0700

    update

---
+++ assignment8/console/migrations/m190912_082901_create_subject_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%subject}}`.
+ */
+class m190912_082901_create_subject_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%subject}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'section' => $this->string(),
+            'teach_id' => $this->integer()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%subject}}');
+    }
+}
