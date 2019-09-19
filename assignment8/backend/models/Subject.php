88bfb2b0eb859ecb082728d76ac5f7e72181b96a
Computer Education <>
2019-09-19 16:21:29 +0700

    update

---
+++ assignment8/backend/models/Subject.php
@@ -0,0 +1,5 @@
+<?php
+namespace backend\models;
+
+class Subject extends \common\models\Subject
+{}
--- assignment8/console/migrations/m190919_082106_create_subject_table.php
+++ assignment8/console/migrations/m190919_082106_create_subject_table.php
@@ -5,7 +5,7 @@ use yii\db\Migration;
 /**
  * Handles the creation of table `{{%subject}}`.
  */
-class m190919_074416_create_subject_table extends Migration
+class m190919_082106_create_subject_table extends Migration
 {
     /**
      * {@inheritdoc}
@@ -14,9 +14,8 @@ class m190919_074416_create_subject_table extends Migration
     {
         $this->createTable('{{%subject}}', [
             'id' => $this->primaryKey(),
-            'name' => $this->string(256)->notNull,
+            'name' => $this->string(256)->notNull(),
             'detail' => $this->text(),
-
         ]);
     }
 
